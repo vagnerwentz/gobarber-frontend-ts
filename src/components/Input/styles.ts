15baa4370070f79b45561f39b6cb98a033633b58
vagnerwentz <wentz.vagner@gmail.com>
2020-06-15 16:31:21 -0300

    feat: border color red to error and error tooltip style

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +23,12 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${(props) =>
     props.isFocused &&
@@ -55,5 +64,23 @@ export const Container = styled.div<ContainerProps>`
   input:-webkit-autofill:active {
     -webkit-box-shadow: 0 0 0 30px #232129 inset !important;
     -webkit-text-fill-color: #f4ede8 !important;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
