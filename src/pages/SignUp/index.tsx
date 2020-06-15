29f6a1afe742a4c9aeab2ad423e8fd09fae7d9e5
vagnerwentz <wentz.vagner@gmail.com>
2020-06-15 12:07:24 -0300

    feat: first input has the wrong content e-mail to nome

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -23,7 +23,7 @@ const SignUp: React.FC = () => {
         <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="E-mail" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
