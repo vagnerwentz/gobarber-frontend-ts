1bd31944242d907c18dd93101481b360e7aa21f5
vagnerwentz <wentz.vagner@gmail.com>
2020-07-19 17:33:21 -0300

    feat: changed button to label to change file

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, ChangeEvent } from 'react';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
@@ -27,7 +27,7 @@ const Profile: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
 
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
 
   const history = useHistory();
 
@@ -76,6 +76,26 @@ const Profile: React.FC = () => {
     [addToast, history],
   );
 
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData();
+
+        data.append('avatar', e.target.files[0]);
+
+        api.patch('/users/avatar', data).then((response) => {
+          updateUser(response.data);
+
+          addToast({
+            type: 'success',
+            title: 'Avatar atualizado.',
+          });
+        });
+      }
+    },
+    [addToast, updateUser],
+  );
+
   return (
     <Container>
       <header>
@@ -96,9 +116,11 @@ const Profile: React.FC = () => {
         >
           <AvatarInput>
             <img src={user.avatar_url} alt={user.name} />
-            <button type="button">
+            <label htmlFor="avatar">
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+
               <FiCamera />
-            </button>
+            </label>
           </AvatarInput>
           <h1>Meu perfil</h1>
 
