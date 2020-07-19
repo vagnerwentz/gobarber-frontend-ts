0599f3c36f8edadbb3e625b34797662a87468a88
vagnerwentz <wentz.vagner@gmail.com>
2020-07-19 17:14:11 -0300

    feat: added profile route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,16 @@ import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
-    <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
+    <Route path="/forgot-password" component={ForgotPassword} />
 
+    <Route path="/profile" component={Profile} isPrivate />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
